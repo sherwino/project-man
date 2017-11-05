08b32de5d07b5cb8ee9c103044b336a11de56623
sherwino <sherwination@gmail.com>
2017-11-05 11:35:55 -0500

    Several changes to edit project  routes

--- models/projectmod.js
+++ models/projectmod.js
@@ -5,8 +5,8 @@ const User = require('./usermod.js');
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema(
-  {
-    jobYear:        Number,
+  { //need to add validations later
+    jobYear:        String,
     jobNumber:      String,
     jobName:        String,
     jobClient:      String,
@@ -34,9 +34,15 @@ const projectSchema = new Schema(
     jobImgName:     String,
     jobImg:         String,
 
+    // along with all of the usual validators max/min you could also specify whether the
+    // field is required...duh, but you can also manipulate strings lowercase: true, trim: true
+    // you could also use enum: to specify set of allowed values, match: to match a regular expression. 
+    // maxlength and minlength can be used on strings too 
+
     //reference the id of the user
-    createdBy:          { type: Schema.Types.ObjectId },
-    updatedBy:          { type: Schema.Types.ObjectId }
+    createdBy:          { type: Schema.Types.ObjectId, ref: 'User' },
+    updatedBy:          { type: Schema.Types.ObjectId, ref: 'User'},
+    updated:            { type: Date, default: Date.now }
 
     // this is the way I would write it if I wanted to make the user a subdoc
     // owner:          { type: User.schema }
--- routes/projects.js
+++ routes/projects.js
@@ -148,13 +148,14 @@ projectRouter.get('/projects/:id/',
 
   (req, res, next ) => {
 
-  const projectID = req.params.id;
+  const projectId = req.params.id;
 
-  Project.findById(projectID, (err, theProject) => {
+  Project.findById(projectId, (err, theProject) => {
     if(err) {
       next(err);
       return;
     }
+
     res.render('projects/project-detail-view.ejs', {
       title:  `Project Man ${theProject.jobNumber}`,
       layout: 'layouts/detail-layout',
@@ -174,75 +175,107 @@ projectRouter.get('/projects/:id/edit',
   //search for the project defined in URL
   // lets start by creating a variable that equal the :id passed in the URL
   const projectId   = req.params.id;
+
   Project.findById(projectId, (err, theProject) => {
     if (err) {
       next(err);
       return;
     }
+  res.locals.project = theProject;
+  req.project = theProject;
   res.render('projects/edit-project-view.ejs', {
     title:    'Edit Project',
     layout:   'layouts/list-layout',
     job:       theProject,
-    errors:    theProject.errors
+    errors:    theProject.errors,
   });
 });
 });
+
 // params gets info from a URL
 // so does query but query requires the url to be in a keyValue pair ?name=bob would be in the URL
 // body gets stuff from inputs
 //
 //                      remeber :id is just a placeholder
 //                      it could be whatever you want
-projectRouter.patch('/projects/:id/edit-project', (req, res, next) => {
-  const projectId = req.params.id;
 
-  const projectChanges = {
-    //the key is from the model, and the value is from the input form
-    jobYear:        req.body.jobYear,
-    jobNumber:      req.body.jobNumber,
-    jobName:        req.body.jobName,
-    jobClient:      req.body.jobClient,
-    jobSubs:        req.body.jobSubs,
-    jobType:        req.body.jobType,
-    jobFee:         req.body.jobFee,
-    jobImg:         `${req.file.location}`,
-    // jobRenderImg:   `/uploads/${req.body.picName}`,
-    jobAddress:     req.body.jobAddress,
-    jobMasterperm:  req.body.jobMasterperm,
-    jobPlbperm:     req.body.jobPlbperm,
-    jobMechperm:    req.body.jobMechperm,
-    jobGasperm:     req.body.jobGasperm,
-    jobElecperm:    req.body.jobElecperm,
-    jobOtherPerm:   req.body.jobOtherPerm,
-    jobChangeorder: req.body.jobChangeorder,
-    jobReimburse:   req.body.jobReimburse,
-    jobPayroll:     req.body.jobPayroll,
-    jobSubExp:      req.body.jobSubExp,
-    jobAmtInv:      req.body.jobAmtInv,
-    jobAmtRec:      req.body.jobAmtRec,
-    jobAmtDue:      req.body.jobAmtDue,
-    jobAmtRem:      req.body.jobAmtRem,
-    jobProfit:      req.body.jobProfit,
-    jobCurrProfit:  req.body.jobCurrProfit,
-    jobMaterialExp: req.body.jobMaterialExp,
-    createdBy:      Project.createdBy, //should add a edited by in the model
-    updatedBy:      req.user._id
-  };
-//this new method has three arguments
-  Project.findByIdAndUpdate(
-    projectId,                  //which document to change
-    projectChanges,             //variable of the changes you want to make
-    (err, theProject) => {      //the callback
-      if (err) {
-        next(err);
-        return;
-      }                        //end of error callback
-                              //this is how you would redired to prodcut details page
-                              // res.redirect(`/projects/${projectId}
-    res.redirect('/projects/:id/');
+
+// POST/SEND NEW PROJECT TO DB ROUTE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+projectRouter.post('/projects/:id/update', 
+
+  myUploader.single('jobImg'),
+
+  (req, res, next) => {
+    const projectId   = req.params.id;
+
+    console.log("Got the post request for: " + projectId);
+
+    console.log('Created By: ' + Project.createdBy);
+
+    const projectChanges = {
+      //the key is from the model, and the value is from the input form
+      //possibly need to put some if statements to handle empty values....
+      jobYear:        req.body.jobYear,
+      jobNumber:      req.body.jobNumber,
+      jobName:        req.body.jobName,
+      jobClient:      req.body.jobClient,
+      jobSubs:        req.body.jobSubs,
+      jobType:        req.body.jobType,
+      jobFee:         req.body.jobFee,
+      // jobImg:         req.body.jobImg,
+      // jobRenderImg:   `/uploads/${req.body.picName}`,
+      jobAddress:     req.body.jobAddress,
+      jobMasterperm:  req.body.jobMasterperm,
+      jobPlbperm:     req.body.jobPlbperm,
+      jobMechperm:    req.body.jobMechperm,
+      jobGasperm:     req.body.jobGasperm,
+      jobElecperm:    req.body.jobElecperm,
+      jobOtherPerm:   req.body.jobOtherPerm,
+      jobChangeorder: req.body.jobChangeorder,
+      jobReimburse:   req.body.jobReimburse,
+      jobPayroll:     req.body.jobPayroll,
+      jobSubExp:      req.body.jobSubExp,
+      jobAmtInv:      req.body.jobAmtInv,
+      jobAmtRec:      req.body.jobAmtRec,
+      jobAmtDue:      req.body.jobAmtDue,
+      jobAmtRem:      req.body.jobAmtRem,
+      jobProfit:      req.body.jobProfit,
+      jobCurrProfit:  req.body.jobCurrProfit,
+      jobMaterialExp: req.body.jobMaterialExp,
+      // createdBy:      req.user.name, //should add a edited by in the model
+      updatedBy:      req.user._id
+
+    };
+
+
+    //check if user uploaded a file, if they did replace the existing value
+    if (req.file) {
+      console.log('FILE UPLOAD ------------------------');
+      console.log('Successfully uploaded ' + req.file.name);
+      projectChanges.jobImg = req.file.location;
+
+    } else {
+      //if they didn't upload or replace the original image
+      console.log('No changes to the image, keeping the on in the DB');
     }
-  );
-});
+
+  //this new method has three arguments
+    Project.findByIdAndUpdate(
+      projectId,                  //which document to change
+      projectChanges,             //variable of the changes you want to make
+      (err, theProject) => {      //the callback
+        console.log('Tried to update the DB');
+        if (err) {
+          next(err);
+          return;
+        }                        //end of error callback
+                                //this is how you would redirect to prodcut details page
+                                // res.redirect(`/projects/${projectId}
+      res.redirect(`/projects/${projectId}/`);
+      console.log(projectChanges);
+      }
+    );
+  });
 
 // DELETE PROJECT PAGE ROUTE  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
